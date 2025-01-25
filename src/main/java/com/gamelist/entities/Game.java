490b26738bbaaa43fca71e6914e4d8390d6572c0
Kevin Vieira <xkevet@outlook.com>
2025-01-25 15:42:41 -0300

    :sparkles: feat: Add ORM on the Game class

--- src/main/java/com/gamelist/entities/Game.java
+++ src/main/java/com/gamelist/entities/Game.java
@@ -2,9 +2,24 @@ package com.gamelist.entities;
 
 import java.util.Objects;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_name")
 public class Game {
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
 private Long id;
 private String title;
+
+@Column(name = "game_year")
 private Integer year;
 private String genre;
 private String platforms;
