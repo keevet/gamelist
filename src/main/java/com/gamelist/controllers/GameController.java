002b1c6512f01062dd5fd17e2a43cbfc2ba72428
Kevin Vieira <xkevet@outlook.com>
2025-01-26 10:53:07 -0300

    :sparkles: feat: Add search for Ids

--- src/main/java/com/gamelist/controllers/GameController.java
+++ src/main/java/com/gamelist/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gamelist.dto.GameDTO;
 import com.gamelist.dto.GameMinDTO;
 import com.gamelist.services.GameService;
 
@@ -17,6 +19,14 @@ public class GameController {
 	
 	@Autowired
 	private GameService gameService;
+	
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id ){
+		
+		return  gameService.findById(id);
+		
+
+	}
 	
 	@GetMapping
 	public List<GameMinDTO> findAll(){
--- src/main/java/com/gamelist/services/GameService.java
+++ src/main/java/com/gamelist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.gamelist.dto.GameDTO;
 import com.gamelist.dto.GameMinDTO;
 import com.gamelist.entities.Game;
 import com.gamelist.repositories.GameRepository;
@@ -15,6 +17,17 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+		
+		
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		
 		List<Game> result = gameRepository.findAll();
