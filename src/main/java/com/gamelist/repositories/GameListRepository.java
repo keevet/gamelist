8cdcb92269324d18cfc3e77a41e68626d5308cff
Kevin Vieira <xkevet@outlook.com>
2025-01-26 11:56:24 -0300

    :sparkles: feat: Add GameList Repository

---
+++ src/main/java/com/gamelist/repositories/GameListRepository.java
@@ -0,0 +1,12 @@
+package com.gamelist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gamelist.entities.GameList;
+
+public interface GameListRepository extends JpaRepository<GameList, Long>{
+
+	
+	
+	
+}
