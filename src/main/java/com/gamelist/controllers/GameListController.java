f1d61edb0203cfe820a06c6a3678f9270f37cda2
Kevin Vieira <xkevet@outlook.com>
2025-01-28 21:53:51 -0300

    :sparkles: feat: Add PostMapping method for switch game position

--- src/main/java/com/gamelist/controllers/GameListController.java
+++ src/main/java/com/gamelist/controllers/GameListController.java
@@ -5,11 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gamelist.dto.GameListDTO;
 import com.gamelist.dto.GameMinDTO;
+import com.gamelist.dto.ReplacementDTO;
 import com.gamelist.services.GameListService;
 import com.gamelist.services.GameService;
 
@@ -40,4 +43,11 @@ public class GameListController {
 
 	}
 	
+	@PostMapping(value = "/{listId}/replacement")
+	public void move(@PathVariable Long listId, @RequestBody ReplacementDTO body){
+		
+		gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
+		
+
+	}
 }
