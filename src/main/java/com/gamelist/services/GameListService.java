82176d5fa62d190a944999d816eaddb648af4eed
Kevin Vieira <xkevet@outlook.com>
2025-01-28 21:25:29 -0300

    :sparkles: feat: Add move method to switch game position in a list

--- src/main/java/com/gamelist/services/GameListService.java
+++ src/main/java/com/gamelist/services/GameListService.java
@@ -4,22 +4,57 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.gamelist.dto.GameListDTO;
 import com.gamelist.entities.GameList;
+import com.gamelist.projections.GameMinProjection;
 import com.gamelist.repositories.GameListRepository;
+import com.gamelist.repositories.GameRepository;
 
 @Service
+
 public class GameListService {
 
 	@Autowired
 	private GameListRepository gameListRepository;
 	
+	@Autowired
+	private GameRepository gameRepository;
+	
+	
+	@Transactional
 	public List<GameListDTO> findAll() {
 		
 		List<GameList> result = gameListRepository.findAll();
 		
 		return result.stream().map(x -> new GameListDTO(x)).toList();
+	}
+	
+	
+	
+	@Transactional
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
+		
+		
+		List<GameMinProjection> list = gameRepository.searchByList(listId);
+		GameMinProjection obj = list.remove(sourceIndex);
+		list.add(destinationIndex, obj);
+		
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+		
+		
+		for(int i = min; i <= max; i++) {
+			
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(),i);
+			
+			
+		}
+		
+		
+		
+		
 	}
 	
 }
